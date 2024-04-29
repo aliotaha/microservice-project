0aca7fa2efc52a2a046e9d7fc3533dbee8b8acc6
3li_7ofra <65508729+aliotaha@users.noreply.github.com>
2024-04-29 13:28:38 +0100

    apigateway
    apigateway

---
+++ apigateway/apigateway/src/main/java/com/gateway/apigateway/ApigatewayApplication.java
@@ -0,0 +1,34 @@
+package com.gateway.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/learn/students/**")
+						.filters( f -> f.rewritePath("/learn/students/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://STUDENTS"))
+				.route(p -> p
+					.path("/learn/teachers/**")
+					.filters( f -> f.rewritePath("/learn/teachers/(?<segment>.*)","/${segment}")
+							.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+					.uri("lb://TEACHERS")).build();
+		
+	}
+}
